f96bf27b990bb635dd980dfab03b7752496f4406
TrungNguyen083 <91688944+TrungNguyen083@users.noreply.github.com>
2023-10-09 08:12:32 +0700

    Update_employee_graphql

--- src/main/java/com/hrms/employeemanagement/specifications/EmployeeSpecifications.java
+++ src/main/java/com/hrms/employeemanagement/specifications/EmployeeSpecifications.java
@@ -1,6 +1,7 @@
 package com.hrms.employeemanagement.specifications;
 
 import com.hrms.employeemanagement.models.Employee;
+import jakarta.annotation.Nullable;
 import org.springframework.data.jpa.domain.Specification;
 
 public class EmployeeSpecifications {
@@ -8,8 +9,23 @@ public class EmployeeSpecifications {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("id"), id);
     }
 
-    public static Specification<Employee> hasUnitId(int unitId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("unit").get("id"), unitId);
+    public static Specification<Employee> hasDepartmentId(int departmentId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("department").get("id"), departmentId);
     }
 
+    public static Specification<Employee> hasPositionLevelId(int positionLevelId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("positionLevel").get("id"), positionLevelId);
+    }
+
+    public static Specification<Employee> hasStatus(boolean isEnabled) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isEnabled"), isEnabled);
+    }
+
+    public static Specification<Employee> hasFilter(@Nullable int departmentId,
+                                                    @Nullable int positionLevelId,
+                                                    @Nullable boolean isEnabled) {
+        return Specification.where(hasDepartmentId(departmentId))
+                .and(hasPositionLevelId(positionLevelId))
+                .and(hasStatus(isEnabled));
+    }
 }

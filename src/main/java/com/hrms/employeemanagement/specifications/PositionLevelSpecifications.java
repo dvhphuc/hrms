50998498bcc1cf5a47b310d248eff42cc6c3e85a
TrungNguyen083 <91688944+TrungNguyen083@users.noreply.github.com>
2023-10-11 10:26:14 +0700

    fix_jobLevel

--- src/main/java/com/hrms/employeemanagement/specifications/PositionLevelSpecifications.java
+++ src/main/java/com/hrms/employeemanagement/specifications/PositionLevelSpecifications.java
@@ -8,6 +8,10 @@ public class PositionLevelSpecifications {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("id"), id);
     }
     public static Specification<PositionLevel> hasPositionIdAndJobLevelID(int positionId, int jobLevelId) {
+        //if jobLevelId == null, then return only positionId
+        if (jobLevelId == 0) {
+            return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("position").get("id"), positionId);
+        }
         return (root, query, criteriaBuilder) -> criteriaBuilder.and(
                 criteriaBuilder.equal(root.get("position").get("id"), positionId),
                 criteriaBuilder.equal(root.get("jobLevel").get("id"), jobLevelId)

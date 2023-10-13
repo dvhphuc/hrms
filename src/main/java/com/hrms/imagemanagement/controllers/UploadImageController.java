bdd66aee185f6db093601473382720457dda6f88
TrungNguyen083 <91688944+TrungNguyen083@users.noreply.github.com>
2023-10-13 11:28:11 +0700

    fix_uploadImage

--- src/main/java/com/hrms/imagemanagement/controllers/UploadImageController.java
+++ src/main/java/com/hrms/imagemanagement/controllers/UploadImageController.java
@@ -3,19 +3,22 @@ package com.hrms.imagemanagement.controllers;
 import com.hrms.employeemanagement.services.EmployeeService;
 import com.hrms.imagemanagement.services.ImageSourceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/employees")
-public class EmployeeController {
+public class UploadImageController {
 
     EmployeeService employeeService;
     ImageSourceService imageSourceService;
     @Autowired
-    public EmployeeController(EmployeeService employeeService, ImageSourceService imageSourceService) {
+    public UploadImageController(EmployeeService employeeService, ImageSourceService imageSourceService) {
         this.employeeService = employeeService;
         this.imageSourceService = imageSourceService;
     }
@@ -28,6 +31,19 @@ public class EmployeeController {
             return ResponseEntity.ok("File uploaded successfully.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Could not upload the file.");
+        }
+    }
+
+    @GetMapping("/{id}/image")
+    public ResponseEntity<Resource> getProfileImage(@PathVariable(value = "id") int id){
+        try {
+            Resource resource = imageSourceService.getProfileImageByEmployeeId(id);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=" + resource.getFilename())
+                    .contentType(MediaType.IMAGE_JPEG)  // Adjust the media type as per your image type
+                    .body(resource);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
         }
     }
 }

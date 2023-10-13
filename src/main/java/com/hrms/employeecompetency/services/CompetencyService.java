a70343650ae1216fd3a50cbfb4f2a7109df9afc4
TrungNguyen083 <91688944+TrungNguyen083@users.noreply.github.com>
2023-10-13 17:07:57 +0700

    fix_departmentNull

---
+++ src/main/java/com/hrms/employeecompetency/services/CompetencyService.java
@@ -0,0 +1,15 @@
+package com.hrms.employeecompetency.services;
+
+import com.hrms.employeecompetency.models.Competency;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+public interface CompetencyService {
+    List<Competency> findAll(Specification<Competency> spec);
+    List<Competency> findAll(Specification<Competency> spec, Sort sort);
+    Page<Competency> findAll(Specification<Competency> spec, Pageable pageable);
+}
